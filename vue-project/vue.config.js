ebc53385fe49f9325c1e04c710a0e2cbb82aaea0
Anne Haley <anne.haley@kitware.com>
2022-08-31 13:40:45 -0400

    Build vue
    Add vuetify to standalone vue project (to mirror trame appearance)

--- vue-project/vue.config.js
+++ vue-project/vue.config.js
@@ -4,6 +4,6 @@ const path = require("path");
 const DST_PATH = "../trame_app/module/serve";
 
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: ["vuetify"],
   outputDir: path.resolve(__dirname, DST_PATH),
 });
